282aa1fd24f211c38436f291b485552888315e56
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-12-06 01:25:52 +0100

    add settings page, auth pages and domail details page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import { onLoginUser } from "@/actions/auth";
+import SideBar from '@/components/sidebar';
+import { ChatProvider } from "@/context/user-chat-context";
+// import SideBar from "@/components/sidebar";
+// import { ChatProvider } from "@/context/user-chat-context";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const OwnerLayout = async ({ children }: Props) => {
+  const authenticated = await onLoginUser();
+  if (!authenticated) return null;
+
+  return (
+    <ChatProvider>
+      <div className="flex h-screen w-full">
+        <SideBar domains={authenticated.domain} />
+        <div className="w-full h-screen flex flex-col px-20 md:px-4">
+          {children}
+        </div>
+      </div>
+    </ChatProvider>
+  );
+};
+
+export default OwnerLayout;
